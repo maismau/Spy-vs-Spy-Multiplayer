16e998a0bc1d300bf498cb750c9ebc5bff6f1b51
Mauricio Maldonado <maismau@users.noreply.github.com>
2026-03-18 17:26:57 -0300

    Fix Express 5 wildcard issue in server index.ts

--- server/index.ts
+++ server/index.ts
@@ -17,7 +17,7 @@ const io = new Server(httpServer, {
 app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 // Fallback to React Router / index.html
-app.get('*', (req, res) => {
+app.use((req, res) => {
     res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
 });
 
